f448cb4764fb2cc90a10803f80857be801c479d5
Oldroy <oldroy.gtd@gmail.com>
2017-11-03 10:15:02 -0600

    Express server initial routes test

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+
+app.get('/temperature', function(req, res) {
+  res.send('24 C')
+})
+
+app.get('/humidity', function(req, res) {
+  res.send('48%')
+})
+
+app.listen(3000, function() {
+  console.log('Server running on port 3000')
+})
