cec117e5e907c11ca7bd25da4b423391d01d26e9
Oldroy <oldroy.gtd@gmail.com>
2017-11-04 09:12:28 -0600

    added cacheing for temps

--- server/index.js
+++ server/index.js
@@ -1,23 +1,15 @@
 const express = require('express')
-const getSensorReadings = require('./get-sensor-readings')
+const getCachedSensorReadings = require('./get-cached-sensor-readings')
 
 const app = express()
 
 
 app.get('/temperature', function(req, res) {
-  getSensorReadings((err, temperature, humidity) => {
-    if (!err) {
-      res.send(temperature.toFixed(1) + ' *C')
-    }
-  })
+  res.send(getCachedSensorReadings.getTemperature().toFixed(1) + ' *C')
 })
 
 app.get('/humidity', function(req, res) {
-  getSensorReadings((err, temperature, humidity) => {
-    if (!err) {
-      res.send(humidity.toFixed(1) + '%')
-    }
-  })
+  res.send(getCachedSensorReadings.getHumidity().toFixed(1) + ' %')
 })
 
 app.listen(3000, function() {
