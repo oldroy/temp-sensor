32b74b638799080ee78b19fdd4e8dafe829e217f
Oldroy <oldroy.gtd@gmail.com>
2017-11-04 12:00:41 -0600

    added humidity charting

--- server/public/script.js
+++ server/public/script.js
@@ -21,6 +21,12 @@ const fetchHumidity = () => {
         return results.json()
       })
       .then(data => {
+        const now = new Date()
+        const timeNow = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds()
+        pushData(humidityChartConfig.data.labels, timeNow, 10)
+        pushData(humidityChartConfig.data.datasets[0].data, data.value, 10)
+        humidityChart.update()
+
         const humidityDisplay = document.getElementById('humidity-display')
         humidityDisplay.innerHTML = '<strong>' + data.value + '</strong>'
       })
@@ -71,3 +77,34 @@ const temperatureChartConfig = {
 }
 
 const temperatureChart = new Chart(temperatureCanvasCtx, temperatureChartConfig)
+
+const humidityCanvasCtx =
+    document.getElementById('humidity-chart').getContext('2d')
+
+const humidityChartConfig ={
+  type:'line',
+  data:{
+    labels:[],
+    datasets:[{
+      data:[],
+      backgroundColor:'rgba(197, 202, 233, 0.5)'
+    }]
+  },
+  options:{
+    legend:{
+      display:false
+    },
+    responsive:true,
+    maintainAspectRatio:false,
+    scales:{
+      yAxes:[{
+        ticks:{
+          suggestedMin:0,
+          suggestedMax:100
+        }
+      }]
+    }
+  }
+}
+
+const humidityChart = new Chart(humidityCanvasCtx, humidityChartConfig)
