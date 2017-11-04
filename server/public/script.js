07c1bb4a1d328888e7c3a4c5d14e61fd5cafad2d
Oldroy <oldroy.gtd@gmail.com>
2017-11-04 09:53:15 -0600

    added fetch script and placeholders for both temp and humidity

---
+++ server/public/script.js
@@ -0,0 +1,28 @@
+const fetchTemperature = () => {
+  fetch('/temperature')
+      .then(results => {
+        return results.text()
+      })
+      .then(text => {
+        const temperatureDisplay =
+            document.getElementById('temperature-display')
+        temperatureDisplay.innerHTML = text
+      })
+}
+
+const fetchHumidity = () => {
+  fetch('/humidity')
+      .then(results => {
+        return results.text()
+      })
+      .then(text => {
+        const temperatureDisplay =
+            document.getElementById('humidity-display')
+        temperatureDisplay.innerHTML = text
+      })
+}
+
+setInterval(() => {
+  fetchTemperature()
+  fetchHumidity()
+}, 5000)
