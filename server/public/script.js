dd6c5607e873e75d0e8235667aba923ac67b2d3e
Oldroy <oldroy.gtd@gmail.com>
2017-11-04 11:43:01 -0600

    added live data and pushData to temp chart

--- server/public/script.js
+++ server/public/script.js
@@ -4,6 +4,12 @@ const fetchTemperature = () => {
         return results.json()
       })
       .then(data => {
+        const now = new Date()
+        const timeNow = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds()
+        pushData(temperatureChartConfig.data.labels, timeNow, 10)
+        pushData(temperatureChartConfig.data.datasets[0].data, data.value, 10)
+        temperatureChart.update()
+
         const temperatureDisplay = document.getElementById('temperature-display')
         temperatureDisplay.innerHTML = '<strong>' + data.value + '</strong>'
       })
@@ -25,51 +31,43 @@ setInterval(() => {
   fetchHumidity()
 }, 5000)
 
+const pushData = (arr, value, maxLen) => {
+  arr.push(value)
+  if(arr.length > maxLen) {
+    arr.shift()
+  }
+}
+
 /**
  * Get the context of the temperature canvas element
  */
 const temperatureCanvasCtx =
     document.getElementById('temperature-chart').getContext('2d')
 
-/**
- * Create a new chart on the context we just instantiated
- */
-const temperatureChart = new Chart(temperatureCanvasCtx,
-    {
-      /**
-       * Were going to show the ongoing temperature as a line chart
-       */
-      type:'line',
-      data:{
-        /**
-         * This is mock data.
-         * The labels, which will form our x-axis, are
-         supposed to represent the time at which each reading was
-         taken.
-         * Finally, we add the dataset, whose data is an
-         array of temperature values.
-         * The background color is set to the same value as
-         the earlier display, with some added transparency (which
-         is why the 'rgba' representation is used)
-         */
-        labels:['10:30','10:31','10:32','10:33'],
-        datasets:[{
-          data:[12,19,23,17],
-          backgroundColor:'rgba(255, 205, 210, 0.5)'
-        }]
-      },
-      options:{
-        /**
-         * There is no need for a legend since there is only
-         one dataset plotted
-         * The 'responsive' and 'maintainAspectRatio' options
-         are set so that the chart takes the width and height of
-         the canvas, and does not set it on its own.
-         */
-        legend:{
-          display:false
-        },
-        responsive:true,
-        maintainAspectRatio:false
-      }
-    })
+const temperatureChartConfig = {
+  type: 'line',
+  data: {
+    labels:[],
+    datasets:[{
+      data:[],
+      backgroundColor:'rgba(255, 205, 210, 0.5)'
+    }]
+  },
+  options: {
+    legend: {
+      display: false
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes:[{
+        ticks:{
+          suggestedMin:10,
+          suggestedMax:40
+        }
+      }]
+    }
+  }
+}
+
+const temperatureChart = new Chart(temperatureCanvasCtx, temperatureChartConfig)
