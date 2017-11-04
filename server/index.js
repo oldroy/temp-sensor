fc2ea7fbd4af03cdcb881f46877a35137f652139
Oldroy <oldroy.gtd@gmail.com>
2017-11-04 05:56:07 -0600

    Added dht to initial server to test gpio and driver

--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
 const express = require('express')
+const sensor = require('node-dht-sensor')
 
 const app = express()
 
 
 app.get('/temperature', function(req, res) {
-  res.send('24 C')
+  sensor.read(22, 12, function (err, temperature, humidity) {
+    if (!err) {
+      res.send(temperature.toFixed(1), + '*C')
+    }
+  })
 })
 
 app.get('/humidity', function(req, res) {
-  res.send('48%')
+  sensor.read(22, 12, function (err, temperature, humidity) {
+    if (!err) {
+      res.send(humidity.toFixed(1), + '%')
+    }
+  })
 })
 
 app.listen(3000, function() {
